332abfa7164d1385e9d12e52006c37503fa51262
Swimtime1 <aiden.crossfield@gmail.com>
2024-02-01 17:08:27 -0500

    bounce

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -16,6 +16,12 @@ public class BallController : MonoBehaviour
 
     // Rigidbody2D Variables
     public Rigidbody2D rb;
+
+    // AudioSource Variables
+    public AudioSource audioSource;
+
+    // AudioClip Variables
+    public AudioClip bounce;
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +35,12 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rb.velocity.y);
+        
+    }
+
+    // Plays a sound on collisions
+    void OnCollisionExit2D(Collision2D col)
+    {
+        audioSource.PlayOneShot(bounce);
     }
 }
