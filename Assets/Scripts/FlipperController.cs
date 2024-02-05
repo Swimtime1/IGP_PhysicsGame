aeb0beccb4e92e2706697a448f3bd9e12e4959e5
Swimtime1 <aiden.crossfield@gmail.com>
2024-02-05 15:14:23 -0500

    left flipper now swings with force

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    // Boolean Variables
+    public bool isLeft;
+
+    // Rigidbody2D Variables
+    public Rigidbody2D lrb/* , rrb */;
+
+    // Float Variables
+    public float force;
+
+    // HingeJoint2D Variables
+    public HingeJoint2D lHinge, rHinge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        force = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyForce();
+    }
+
+    // Applies force to the flipper
+    void ApplyForce()
+    {
+        // Adds Force to left Flipper
+        if(Input.GetKey(KeyCode.A))
+        {
+            lHinge.useMotor = false;
+            lrb.AddForce(transform.up * force, ForceMode2D.Impulse);
+        }
+        else if(Input.GetKeyUp(KeyCode.A))
+        {
+            lHinge.useMotor = true;
+            lrb.AddForce(-transform.up * force, ForceMode2D.Impulse);
+        }
+
+        // Adds Force to right Flipper
+        /* if(Input.GetKey(KeyCode.D))
+        {
+            rHinge.useMotor = false;
+            rrb.AddForce(transform.up * force, ForceMode2D.Impulse);
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            rHinge.useMotor = true;
+            rrb.AddForce(-transform.up * force, ForceMode2D.Impulse);
+        } */
+    }
+}
